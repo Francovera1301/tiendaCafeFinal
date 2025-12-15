9e80508ea3e3c18447f309d6e422b219bfc38a59
Franco <francovera1301@gmail.com>
2025-12-15 11:28:45 -0300

    Reestructuracion completa del proyecto

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(session({
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 //app.use("/public", express.static(path.join(__dirname, "public")));
 //app.use("/uploads", express.static(path.join(__dirname, "uploads")));
